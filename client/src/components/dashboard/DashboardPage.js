24bd458246e2e86c32a9070c5e2326a0da26bc16
Joseph S <joseph.segarra@developersinstitute.co.nz>
2023-08-28 16:19:16 +1200

    sending access tokens to Dashboard Page

--- client/src/components/dashboard/DashboardPage.js
+++ client/src/components/dashboard/DashboardPage.js
@@ -4,6 +4,7 @@ import Loader from "../Loader";
 import ErrorMessage from "../ErrorMessage";
 import CategoriesReport from "./CategoriesReport";
 import DiscountsReport from "./DiscountReport";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const DashboardPage = () => {
   const [loading, setLoading] = useState(false);
@@ -11,7 +12,8 @@ const DashboardPage = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [categoryReports, setCategoryReport] = useState([]);
   const [discountReports, setDiscountReport] = useState([]);
-  const [product, setProduct] = useState("");
+
+  const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     // We use AbortController (https://developer.mozilla.org/en-US/docs/Web/API/AbortController)
@@ -24,7 +26,8 @@ const DashboardPage = () => {
         setLoading(true);
         setError(false);
         setErrorMessage("");
-        const result = await api.getReports();
+        const accessToken = await getAccessTokenSilently();
+        const result = await api.getReports(accessToken);
         if (!result.ok) {
           const error = await result.json();
           throw new Error(error.message || "Error fetching reports");
@@ -52,7 +55,7 @@ const DashboardPage = () => {
   }, []);
 
   return (
-    <main className="narrow-layout main-content section-padding page-padding">
+    <main className='narrow-layout main-content section-padding page-padding'>
       <div>
         <h2>Dashboard</h2>
         {loading && <Loader />}
@@ -60,13 +63,13 @@ const DashboardPage = () => {
         {categoryReports.length > 0 && (
           <CategoriesReport
             reportData={categoryReports}
-            className="half-width section-padding"
+            className='half-width section-padding'
           />
         )}
         {discountReports.length > 0 && (
           <DiscountsReport
             reportData={discountReports}
-            className="half-width section-padding"
+            className='half-width section-padding'
           />
         )}
       </div>
