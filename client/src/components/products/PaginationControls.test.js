82c4df2452bff8dfca0e3fa680c4eae8bd30e94c
Joseph S <joseph.segarra@developersinstitute.co.nz>
2023-08-30 16:55:34 +1200

    all test suites passing

--- client/src/components/products/PaginationControls.test.js
+++ client/src/components/products/PaginationControls.test.js
@@ -4,56 +4,41 @@ import PaginationControls from "./PaginationControls";
 
 describe("PaginationControls", () => {
   test("WHEN the user is on the first page of the products page, THEN the previous/back button of the pagination control will be disabled", () => {
-    const onPrev = jest.fn();
+    const setPage = jest.fn();
     render(
-      <PaginationControls
-        onNext={() => {}}
-        onPrev={onPrev}
-        currentPage={1}
-        totalPages={5}
-      />
+      <PaginationControls setPage={setPage} currentPage={1} totalPages={5} />
     );
     const previousButton = screen.getByRole("button", {
       name: "Previous page",
     });
     expect(previousButton).toBeDisabled();
     userEvent.click(previousButton);
-    expect(onPrev).not.toHaveBeenCalled();
+    expect(setPage).not.toHaveBeenCalled();
   });
 
   test("WHEN the user is on the second page of the main product page, THEN the previous/back button of the pagination control will be enabled.", () => {
-    const onPrev = jest.fn();
+    const setPage = jest.fn();
     render(
-      <PaginationControls
-        onNext={() => {}}
-        onPrev={onPrev}
-        currentPage={2}
-        totalPages={5}
-      />
+      <PaginationControls setPage={setPage} currentPage={2} totalPages={5} />
     );
     const previousButton = screen.getByRole("button", {
       name: "Previous page",
     });
     expect(previousButton).not.toBeDisabled();
     userEvent.click(previousButton);
-    expect(onPrev).toHaveBeenCalled();
+    expect(setPage).toHaveBeenCalled();
   });
 
   test("WHEN the user is on the last page of the main product page, THEN next button of the pagination control will be disabled.", () => {
-    const onNext = jest.fn();
+    const setPage = jest.fn();
     render(
-      <PaginationControls
-        onNext={onNext}
-        onPrev={() => {}}
-        currentPage={5}
-        totalPages={5}
-      />
+      <PaginationControls setPage={setPage} currentPage={5} totalPages={5} />
     );
     const nextButton = screen.getByRole("button", {
       name: "Next page",
     });
     expect(nextButton).toBeDisabled();
     userEvent.click(nextButton);
-    expect(onNext).not.toHaveBeenCalled();
+    expect(setPage).not.toHaveBeenCalled();
   });
 });
